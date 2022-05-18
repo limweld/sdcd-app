aa5dc29fecb9e5e2237be2840f63aa51d4775755
limweld <libradilla.limwel@gmail.com>
2022-05-18 17:36:05 +0800

    Facemask Detector

--- src/main/app/web/spa/controller/home.js
+++ src/main/app/web/spa/controller/home.js
@@ -101,7 +101,6 @@ angular.module('Main')
 
 		$scope.home.aiPanel.faceMaskDetectionModeClickPanel  = function(){
 			defaultAIPanelFunctionType("FM");
-			insertSnapshot();
 		}
 
 		let idIndex = 0;
@@ -130,7 +129,6 @@ angular.module('Main')
 
 						selectdefaultCam("default");
 						getCodecInfo();
-						insertSnapshot();
 					}
                 }
             );
@@ -266,37 +264,36 @@ angular.module('Main')
 		var ctx1 = canvassd.getContext('2d');
 		var ctx2 = canvasfm.getContext('2d');
 
-		// set canvas size = video size when known
-		video.addEventListener('loadedmetadata', function() {
-			canvassd.width = $scope.home.cwidth;
-			canvassd.height = $scope.home.clength;
-			canvasfm.width = $scope.home.cwidth;
-			canvasfm.height = $scope.home.clength;
-		});
+		// // set canvas size = video size when known
+		// video.addEventListener('loadedmetadata', function() {
+		// 	canvassd.width = $scope.home.cwidth;
+		// 	canvassd.height = $scope.home.clength;
+		// 	canvasfm.width = $scope.home.cwidth;
+		// 	canvasfm.height = $scope.home.clength;
+		// });
 
-		video.addEventListener('play', function() {
-		var $this = this; //cache
-		(
-			function loop() {
-				if (!$this.paused && !$this.ended) {
-					ctx1.drawImage($this, 0, 0, $scope.home.cwidth, $scope.home.clength);
-					ctx2.drawImage($this, 0, 0, $scope.home.cwidth, $scope.home.clength);
-					setTimeout(loop, 1000 / 30); // drawing at 30fps
-				}
-			}
-		)();
-		}, 0);
+		// video.addEventListener('play', function() {
+		// var $this = this; //cache
+		// (
+		// 	function loop() {
+		// 		if (!$this.paused && !$this.ended) {
+		// 			ctx1.drawImage($this, 0, 0, $scope.home.cwidth, $scope.home.clength);
+		// 			ctx2.drawImage($this, 0, 0, $scope.home.cwidth, $scope.home.clength);
+		// 			setTimeout(loop, 1000 / 30); // drawing at 30fps
+		// 		}
+		// 	}
+		// )();
+		// }, 0);
 
 		let clientId = "clinetId";
 
 		// Create a client instance
-		let client = new Paho.MQTT.Client("192.168.1.150", 9001, clientId);
+		let client = new Paho.MQTT.Client("192.168.2.150", 9001, clientId);
 
 		// set callback handlers
 		client.onConnectionLost = onConnectionLost;
 		client.onMessageArrived = onMessageArrived;
 
-		// connect the client
 		client.connect({
 			timeout: 10,
 			keepAliveInterval: 20,
@@ -311,49 +308,243 @@ angular.module('Main')
 		});
 
 
-		// called when the client connects
 		function onConnect() {
-		// Once a connection has been made, make a subscription and send a message.
 			console.log("onConnect");
 			client.subscribe("soundalarm");
+			client.subscribe("mask_interval");
+		}
 
-		
+	
+
+		let triggerSound = function(){
+			let data = {"channel": 0 };
+			let dataJSON = JSON.stringify(data);
+			let message = new Paho.MQTT.Message(dataJSON);
+			message.destinationName = "soundalarm";
+			client.send(message);
 		}
 
 
-		// let triggerSound = function(){
-		// 	let data = {"channel": $scope.home.devices.selected.id };
-		// 	let dataJSON = JSON.stringify(data);
-
-
-		// 	let message = new Paho.MQTT.Message(dataJSON);
-		// 	message.destinationName = "soundalarm";
-		// 	client.send(message);
-		// }
-
-
-		// // called when the client loses its connection
 		function onConnectionLost(responseObject) {
 			if (responseObject.errorCode !== 0) {
 				console.log("onConnectionLost:"+responseObject.errorMessage);
 			}
 		}
 
-		// called when a message arrives
 		function onMessageArrived(message) {
-			console.log("onMessageArrived:"+message.payloadString);
+	
+			if(message.destinationName == "mask_interval"){
+
+				let data = JSON.parse(message.payloadString);
+				if(data.posting == 0){
+					console.log(data.posting);
+					insertSnapshot();
+				}
+			}
+	
 		}
 
 
 		let insertSnapshot = function(){
-			let canvas1 = document.getElementById('canvassd');
-			let dataURL = canvas1.toDataURL();
-			// console.log(dataURL);
-	
+			let canvas1 = document.getElementById('canvasfm');
+			let dataURL = canvas1.toDataURL();	
 			archivesCreate($scope.home.devices.selected.value,dataURL);
 		}
 
+
+
+
+
+		//triggerSound();
+
+
+		/** Facemask Detector */
 	
+		let model;
+
+		let id2class = {0:"FaceMask", 1:"No_FaceMask"};
+
+		let detectImage = function() {
+			
+			detect(video).then((results) => {
+				canvasfm.width = video.width;
+				canvasfm.height = video.height;
+				ctx2.clearRect(0, 0, canvasfm.width, canvasfm.height);
+				ctx2.drawImage(video, 0, 0, video.width, video.height);
+				
+				results.forEach(bboxInfoFunc);
+
+			})
+		}
+
+
+		function bboxInfoFunc(bboxInfo, index) {
+				let bbox = bboxInfo[0];
+				let classID = bboxInfo[1];
+				let score = bboxInfo[2];
+				ctx2.beginPath();
+				ctx2.lineWidth="4";
+				if (classID == 0) {
+					ctx2.strokeStyle="green";
+					ctx2.fillStyle="green";
+				} else {
+					ctx2.strokeStyle="red";
+					ctx2.fillStyle="red";
+					triggerSound();
+			
+				}
+				
+				ctx2.rect(bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]);
+				ctx2.stroke();
+				
+				ctx2.font="15px Arial";
+				
+
+				let content = id2class[classID] + " " + score.toFixed(2);
+				
+				ctx2.fillText(content, bbox[0], bbox[1] < 20 ? bbox[1] + 30 : bbox[1]-5);
+				
+		}
+
+		async function detect(imgToPredict) {
+			const detectionResults = tf.tidy(() => {
+				// eslint-disable-next-line
+				const width = imgToPredict.width;
+				// eslint-disable-next-line
+				const height = imgToPredict.height;
+				let img = tf.browser.fromPixels(imgToPredict);
+				img = tf.image.resizeBilinear(img, [260, 260]);
+				img = img.expandDims(0).toFloat().div(tf.scalar(255));
+				const [rawBBoxes, rawConfidences] = model.predict(img);
+				const bboxes = decodeBBox(anchors, tf.squeeze(rawBBoxes));
+				const Results = nonMaxSuppression(bboxes, tf.squeeze(rawConfidences), 0.5, 0.5,  width, height );
+				return Results;
+			})
+			return detectionResults;
+		}
+
+		// decode the output according to anchors
+		function decodeBBox(anchors, rawOutput, variances=[0.1,0.1,0.2,0.2]) {
+			const [anchorXmin, anchorYmin, anchorXmax, anchorYmax] = tf.split(anchors, [1,1,1,1], -1);
+			const anchorCX = tf.div(tf.add(anchorXmin, anchorXmax), 2);
+			const anchorCY = tf.div(tf.add(anchorYmin, anchorYmax), 2);
+
+			const anchorW = tf.sub(anchorXmax, anchorXmin);
+			const anchorH = tf.sub(anchorYmax, anchorYmin);
+
+			const rawOutputScale = tf.mul(rawOutput, tf.tensor(variances));
+			const [rawOutputCX, rawOutputCY, rawOutputW, rawOutputH] = tf.split(rawOutputScale, [1,1,1,1], -1);
+			const predictCX = tf.add(tf.mul(rawOutputCX, anchorW), anchorCX);
+			const predictCY = tf.add(tf.mul(rawOutputCY,anchorH), anchorCY);
+			const predictW = tf.mul(tf.exp(rawOutputW), anchorW);
+			const predictH = tf.mul(tf.exp(rawOutputH), anchorH);
+			const predictXmin = tf.sub(predictCX, tf.div(predictW, 2));
+			const predictYmin = tf.sub(predictCY, tf.div(predictH, 2));
+			const predictXmax = tf.add(predictCX, tf.div(predictW, 2));
+			const predictYmax = tf.add(predictCY, tf.div(predictH, 2));
+			// eslint-disable-next-line
+			const predictBBox = tf.concat([predictYmin, predictXmin, predictYmax, predictXmax],-1);
+			return predictBBox
+		}
+
+		// generate anchors
+		function anchorGenerator(featureMapSizes, anchorSizes, anchorRatios) {
+			let anchorBBoxes = [];
+			// eslint-disable-next-line
+			featureMapSizes.map((featureSize, idx) =>{
+				const cx = tf.div(tf.add(tf.linspace(0, featureSize[0] - 1, featureSize[0]) , 0.5) , featureSize[0]);
+				const cy = tf.div(tf.add(tf.linspace(0, featureSize[1] - 1, featureSize[1]) , 0.5) , featureSize[1]);
+				const cxGrid = tf.matMul(tf.ones([featureSize[1], 1]), cx.reshape([1,featureSize[0]]));
+				const cyGrid = tf.matMul(cy.reshape([featureSize[1], 1]), tf.ones([1, featureSize[0]]));
+				// eslint-disable-next-line
+				const cxGridExpend = tf.expandDims(cxGrid, -1);
+				// eslint-disable-next-line
+				const cyGridExpend = tf.expandDims(cyGrid, -1);
+				// eslint-disable-next-line
+				const center = tf.concat([cxGridExpend, cyGridExpend], -1);
+				const numAnchors = anchorSizes[idx].length + anchorRatios[idx].length -1;
+				const centerTiled = tf.tile(center, [1, 1, 2*numAnchors]);
+				// eslint-disable-next-line
+				let anchorWidthHeights = [];
+				
+				// eslint-disable-next-line
+				for (const scale of anchorSizes[idx]) {
+					const ratio = anchorRatios[idx][0];
+					const width = scale * Math.sqrt(ratio);
+					const height = scale / Math.sqrt(ratio);
+
+					const halfWidth = width /  2;
+					const halfHeight = height / 2;
+					anchorWidthHeights.push(-halfWidth, -halfHeight, halfWidth, halfHeight);
+				}
+
+				// eslint-disable-next-line
+				for ( const ratio of anchorRatios[idx].slice(1)) {
+					const scale = anchorSizes[idx][0];
+					const width = scale * Math.sqrt(ratio);
+					const height = scale / Math.sqrt(ratio);
+					const halfWidth = width /  2;
+					const halfHeight = height / 2;
+					anchorWidthHeights.push(-halfWidth, -halfHeight, halfWidth, halfHeight);
+				}
+				const bboxCoord = tf.add(centerTiled , tf.tensor(anchorWidthHeights));
+				const bboxCoordReshape = bboxCoord.reshape([-1, 4]);
+				anchorBBoxes.push(bboxCoordReshape);
+			})
+			// eslint-disable-next-line
+			anchorBBoxes = tf.concat(anchorBBoxes, 0);
+			return anchorBBoxes;
+		}
+
+		let featureMapSizes = [[33, 33], [17, 17], [9, 9], [5, 5], [3,3]];
+
+
+		let anchorSizes =  [[0.04, 0.056], [0.08, 0.11], [0.16, 0.22], [0.32, 0.45], [0.64, 0.72]];
+		let anchorRatios = [[1, 0.62, 0.42], [1, 0.62, 0.42], [1, 0.62, 0.42], [1, 0.62, 0.42], [1, 0.62, 0.42]];
+		let anchors = anchorGenerator(featureMapSizes, anchorSizes, anchorRatios);
+	
+
+		//  nms function
+		let nonMaxSuppression = function(bboxes, confidences, confThresh, iouThresh, width, height, maxOutputSize=100) {
+			const bboxMaxFlag = tf.argMax(confidences, -1);
+			const bboxConf = tf.max(confidences, -1);
+			const keepIndices = tf.image.nonMaxSuppression(bboxes, bboxConf, maxOutputSize, iouThresh, confThresh);
+			// eslint-disable-next-line
+			let results = []
+			const keepIndicesData = keepIndices.dataSync();
+			const bboxConfData = bboxConf.dataSync();
+			const bboxMaxFlagData = bboxMaxFlag.dataSync();
+			const bboxesData = bboxes.dataSync();
+			// eslint-disable-next-line
+			keepIndicesData.map((idx) => {
+				const xmin = Math.round(Math.max(bboxesData[4*idx + 1] * width, 0));
+				const ymin = Math.round(Math.max(bboxesData[4*idx + 0] * height, 0));
+				const xmax = Math.round(Math.min(bboxesData[4*idx+3] * width, width))
+				const ymax = Math.round(Math.min(bboxesData[4*idx + 2] * height, height));
+				results.push([[xmin, ymin, xmax, ymax],
+					bboxMaxFlagData[idx], bboxConfData[idx]])
+			});
+			return results;
+		}
+
+	
+		video.addEventListener("loadeddata", async () => {
+			setInterval(detectImage, 40);
+			//detectImage();
+		});
+
+		async function loadModel() {
+			 //model = await tf.loadLayersModel('https://raw.githubusercontent.com/alifwahyd/FaceMaskDetection-Webapp/master/tfjs-models/model.json');
+			model = await tf.loadLayersModel('ai/models/mask_models/model.json');
+			return model;
+		}
+
+		async function setup() {
+			await loadModel();
+		}
+
+		setup();
+
 	}
 
 ]);
